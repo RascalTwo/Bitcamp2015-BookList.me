c3722daaa406d0084b59a1a069544776a4697586
RascalTwo <stewartlittle58@gmail.com>
2015-04-11 03:22:37 -0400

    Added accounts and basic posts

--- Bitcamp2015.js
+++ Bitcamp2015.js
@@ -1,7 +1,56 @@
+postCollection = new Mongo.Collection('posts')
+
 if (Meteor.isClient) {
+
+
+	Template.body.helpers({
+		loggedIn: function (){
+			if (Meteor.userId() == null){
+				return false
+			}
+			else{
+				return true
+			}
+		},
+		posts: function (){
+			return postCollection.find({}, {sort: {postDate: -1}})
+		}
+	})
+
+	Template.body.events({
+		'submit #submissionForm': function (event){
+			event.preventDefault()
+			fields = [event.target.bookName.value, event.target.isbn.value, event.target.quality.value, event.target.askingPrice.value]
+			makePost = true
+			for (var i = 0; i < fields.length; i++){
+				if (fields[i] == ''){
+					makePost = false
+				}
+			}
+			if (makePost == false){
+				alert('All Fields Must Be Filled Out!')
+			}
+			else{
+				postCollection.insert({
+					bookName: fields[0],
+					isbn: fields[1],
+					quality: fields[2],
+					askingPrice: fields[3],
+					posterId: Meteor.userId(),
+					posterName: Meteor.user().username,
+					postDate: new Date()
+				})
+			}
+		}
+	})
+
+	Accounts.ui.config({
+		passwordSignupFields: 'USERNAME_ONLY'
+	})
 }
 
 if (Meteor.isServer) {
-	Meteor.startup(function () {
-	});
+	Meteor.startup(function (){
+
+	})
 }
